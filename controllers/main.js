ca6144b733efedf750846088a53d5a48ca0f1851
tony-sn <tony.sn@outlook.com>
2021-05-29 22:40:13 +1000

    render html file

--- controllers/main.js
+++ controllers/main.js
@@ -4,9 +4,37 @@ $(document).ready(() => {
   const renderHTML = () => {
     callData
       .getListData()
-      .done((result) => console.log(result))
+      .done((result) => {
+        console.log(result.navPills);
+        let contentNavPills = "";
+        let contentTabPanes = "";
+        result.navPills.forEach((item, index) => {
+          let activeClass = item.tabName === "tabTopClothes" ? "active" : "";
+          let fadeClass = item.tabName !== "tabTopClothes" ? "fade" : "";
+
+          contentNavPills += getElmTabPill(item, activeClass);
+          contentTabPanes += `
+          <div class ="tab-pane container ${activeClass} ${fadeClass}" id="${item.tabName}">
+            <div class="row">
+              ${item.showName}
+            </div>          
+          </div>
+          `;
+        });
+
+        $(".nav-pills").html(contentNavPills);
+        $(".tab-content").html(contentTabPanes);
+      })
       .fail((err) => console.log(err));
   };
 
   renderHTML();
+
+  const getElmTabPill = (item, activeClass) => {
+    return `
+    <li class ="nav-item">
+      <a class ="nav-link ${activeClass} btn-default" data-toggle="pill" href="${item.tabName}">${item.showName}</a>   
+    </li>
+    `;
+  };
 });
