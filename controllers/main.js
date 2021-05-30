02aabf1fcce8aba9062b153c6828bdce09edeb41
tony-sn <tony.sn@outlook.com>
2021-05-30 11:48:47 +1000

    change main.js

--- controllers/main.js
+++ controllers/main.js
@@ -1,6 +1,87 @@
 $(document).ready(() => {
   let callData = new CallData();
 
+  const getElmTabPill = (item, activeClass) => {
+    return `
+    <li class ="nav-item">
+      <a class ="nav-link ${activeClass} btn-default" data-toggle="pill" href="${item.tabName}">${item.showName}</a>   
+    </li>
+    `;
+  };
+
+  const getTypeArr = (tabType, data) => {
+    let tempArr = [];
+    data.forEach((item) => {
+      console.log("item", item);
+      if (item.type === tabType) {
+        tempArr.push(item);
+      }
+    });
+    return tempArr;
+  };
+
+  const getElmItem = (tempArr) => {
+    var elmItem = "";
+    tempArr.forEach((item) => {
+      elmItem += `
+      <div class="col-md-3">
+        <div class="card text-center">
+        <img src="${item.imgSrc_jpg}">
+        <h4><b>${item.name}</b></h4>
+        <button>Change</button>
+        </div> 
+      </div>
+      `;
+    });
+    return elmItem;
+  };
+
+  const renderTabPane = (tabName, arrayTabPane) => {
+    let tempArr = null;
+    let elmItem = null;
+    switch (tabName) {
+      case "tabTopClothes":
+        tempArr = getTypeArr("topclothes", arrayTabPane);
+        elmItem = getElmItem(tempArr);
+        break;
+
+      case "tabBotClothes":
+        tempArr = getTypeArr("botclothes", arrayTabPane);
+        elmItem = getElmItem(tempArr);
+        break;
+
+      case "tabShoes":
+        tempArr = getTypeArr("shoes", arrayTabPane);
+        elmItem = getElmItem(tempArr);
+        break;
+
+      case "tabHandBags":
+        tempArr = getTypeArr("handbags", arrayTabPane);
+        elmItem = getElmItem(tempArr);
+        break;
+
+      case "tabNecklaces":
+        tempArr = getTypeArr("necklaces", arrayTabPane);
+        elmItem = getElmItem(tempArr);
+        break;
+
+      case "tabHairStype":
+        tempArr = getTypeArr("hairstyle", arrayTabPane);
+        elmItem = getElmItem(tempArr);
+        break;
+
+      case "tabBotClothes":
+        break;
+
+      default:
+        tempArr = getTypeArr("background", arrayTabPane);
+        elmItem = getElmItem(tempArr);
+
+        break;
+    }
+    return elmItem;
+  };
+
   const renderHTML = () => {
     callData
       .getListData()
@@ -14,9 +95,11 @@ $(document).ready(() => {
 
           contentNavPills += getElmTabPill(item, activeClass);
           contentTabPanes += `
-          <div class ="tab-pane container ${activeClass} ${fadeClass}" id="${item.tabName}">
+          <div class ="tab-pane container ${fadeClass} ${activeClass}" id="${
+            item.tabName
+          }">
             <div class="row">
-              ${item.showName}
+              ${renderTabPane(item.tabName, item.tabPanes)}
             </div>          
           </div>
           `;
@@ -29,12 +112,4 @@ $(document).ready(() => {
   };
 
   renderHTML();
-
-  const getElmTabPill = (item, activeClass) => {
-    return `
-    <li class ="nav-item">
-      <a class ="nav-link ${activeClass} btn-default" data-toggle="pill" href="${item.tabName}">${item.showName}</a>   
-    </li>
-    `;
-  };
 });
